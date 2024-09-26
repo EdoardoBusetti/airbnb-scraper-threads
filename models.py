51865ffb812a9a8057f0d56ef13295feaddb0bc5
Edoardo Busetti <edoardobusetti95@gmail.com>
2024-09-26 22:35:05 +0200

    Add state transitions model

--- models.py
+++ models.py
@@ -113,20 +113,15 @@ class AirBnbRoomCalendarDay(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, onupdate=func.now())
     state = Column(String)
-    previous_state = Column(String)
-    minimum_stay_nights = Column(Integer)
     price = Column(
         Float,
         comment="It is the average price for the prices which this day has on the latest evaluation run. [one day can be in multiple stays intervals. this will be avg price for that room in those stay intervals]",
-    )
-    previous_price = Column(
-        Float,
-        comment="previous price",
     )
     latest_prices_array = Column(
         JSON,
         comment="Array with the prices from current run. with their checking and checkout dates. [{'check_in':XXX,'check_out':YYY, 'price':price1},{'check_in':ZZZ,'check_out':KKK, 'price':price2}]",
     )
+    minimum_stay_nights = Column(Integer)
     cleaning_fee = Column(Float)
     currency = Column(String)
     extra_attributes = Column(
@@ -159,22 +154,52 @@ class AirBnbRoomCalendarDayTransition(Base):
 
     __tablename__ = "airbnb_room_calendar_day_transitions"
     room_id = Column(String, primary_key=True)
+    calendar_day = Column(Date, primary_key=True)
     created_at = Column(
         DateTime,
         default=func.now(),
-        comment="Timestamp when the room calendar update was created",
+        comment="Timestamp when the room calendar transition was recorded",
     )
-    any_changes = Column(
-        Boolean, comment="True if there were any changes compared to previous version"
+    transition_type = Column(String)
+    state = Column(String)
+    price = Column(Float)
+    latest_prices_array = Column(
+        JSON,
+        comment="Array with the prices from current run. with their checking and checkout dates. [{'check_in':XXX,'check_out':YYY, 'price':price1},{'check_in':ZZZ,'check_out':KKK, 'price':price2}]",
     )
-    previous_state = Column(String)
-    current_state = Column(String)
-    previous_price = Column(Float)
-    current_price = Column(Float)
+    minimum_stay_nights = Column(Integer)
+    cleaning_fee = Column(Float)
+    currency = Column(String)
     extra_attributes = Column(
         JSON,
         comment="Json with extra attributes",
     )
+
+
+def check_calendar_day_changes(
+    existing_instance, new_instance, price_change_tolerance_pct
+):
+    state_change = existing_instance.state != new_instance.state
+    min_nights_change = (
+        existing_instance.minimum_stay_nights != new_instance.minimum_stay_nights
+    )
+    price_change = (
+        abs(existing_instance.price - new_instance.price) / existing_instance.price
+    ) >= price_change_tolerance_pct
+    cleaning_fee_change = existing_instance.cleaning_fee != new_instance.cleaning_fee
+    currency_change = (
+        existing_instance.currency != new_instance.currency
+    )  # should not be unless IP location changes
+    any_change = any(
+        [
+            state_change,
+            min_nights_change,
+            price_change,
+            cleaning_fee_change,
+            currency_change,
+        ]
+    )
+    return any_change, state_change
 
 
 def save_or_update_airbnb_room_instance(instance, session):
@@ -186,82 +211,88 @@ def save_or_update_airbnb_room_instance(instance, session):
         session.add(instance)
 
 
-def save_or_update_airbnb_date(new_instance: AirBnbRoomCalendarDay, session):
+def save_or_update_airbnb_date(
+    new_instance: AirBnbRoomCalendarDay,
+    session,
+    price_change_tolerance_pct: float = 0.1,
+):
+    """when passed a new_instance of a calendar day, it checks if this was already stored in db previously.
+    if not, stores it and creates NEW_DATE_RECORDED transition.
+    if yes, will check if there was any change for that day compared to previous record.
+    if there is no change, stores the new information.
+    if there is a change, will update the record
+
+    Args:
+        new_instance (AirBnbRoomCalendarDay): the newly created instance of AirBnbRoomCalendarDay from current job run
+        session (Any): the db session
+        price_change_tolerance_pct (float, optional): the tolerance for the price change (given that price change might just be due to rounding or to different days being included in the price array). Defaults to 0.1 [10%].
+
+    """
     existing_instance: AirBnbRoomCalendarDay = (
         session.query(AirBnbRoomCalendarDay)
         .filter_by(room_id=new_instance.room_id, calendar_day=new_instance.calendar_day)
         .first()
     )
     if existing_instance:
-        if existing_instance.state == new_instance.state:
-            existing_instance.previous_state = new_instance.state
-            existing_instance.minimum_stay_nights = (
-                new_instance.minimum_stay_nights
-                if new_instance.minimum_stay_nights
-                else existing_instance.minimum_stay_nights
+        any_change, state_change = check_calendar_day_changes(
+            existing_instance, new_instance, price_change_tolerance_pct
+        )
+        if (
+            any_change
+        ):  # change. store changes and generate AirBnbRoomCalendarDayTransition
+            transition_type = (
+                existing_instance.state + " - " + new_instance.state
+                if state_change
+                else "ATTRIBUTES_ONLY_CHANGE"
             )
-            existing_instance.previous_price = existing_instance.price
-            existing_instance.price = (
+
+            # some attributes might not longer be present in new state (will be nulls) in that case, we just keep previous atributes.
+            new_instance.price = (
                 new_instance.price if new_instance.price else existing_instance.price
             )
-            existing_instance.latest_prices_array = (
-                new_instance.latest_prices_array
-                if new_instance.latest_prices_array
-                else existing_instance.latest_prices_array
-            )
-            existing_instance.cleaning_fee = (
-                new_instance.cleaning_fee
-                if new_instance.cleaning_fee
-                else existing_instance.cleaning_fee
-            )
-            existing_instance.currency = (
+            new_instance.currency = (
                 new_instance.currency
                 if new_instance.currency
                 else existing_instance.currency
             )
-            existing_instance.extra_attributes = {
-                **existing_instance.extra_attributes,
-                **new_instance.extra_attributes,
-            }  # Merge dictionaries, with new_instance's values prevailing in case of conflict
-            session.merge(
-                existing_instance
-            )  # the new details will override the old  ones
-        elif (
-            existing_instance.state != new_instance.state
-        ):  # TODO. Map each individual possible state transition and generate different AirBnbRoomCalendarDayTransition for each of those. (e. from AVAILABLE to UNAVAILABLE_DUE_TO_PAST_DATE. it is unbooked date)
-            existing_instance.previous_state = new_instance.state
-            existing_instance.minimum_stay_nights = (
-                new_instance.minimum_stay_nights
-                if new_instance.minimum_stay_nights
-                else existing_instance.minimum_stay_nights
-            )
-            existing_instance.previous_price = existing_instance.price
-            existing_instance.price = (
-                new_instance.price if new_instance.price else existing_instance.price
-            )
-            existing_instance.latest_prices_array = (
-                new_instance.latest_prices_array
-                if new_instance.latest_prices_array
-                else existing_instance.latest_prices_array
-            )
-            existing_instance.cleaning_fee = (
+            new_instance.cleaning_fee = (
                 new_instance.cleaning_fee
                 if new_instance.cleaning_fee
                 else existing_instance.cleaning_fee
             )
-            existing_instance.currency = (
-                new_instance.currency
-                if new_instance.currency
-                else existing_instance.currency
+            new_instance.minimum_stay_nights = (
+                new_instance.minimum_stay_nights
+                if new_instance.minimum_stay_nights
+                else existing_instance.minimum_stay_nights
             )
-            existing_instance.extra_attributes = {
-                **existing_instance.extra_attributes,
-                **new_instance.extra_attributes,
-            }  # Merge dictionaries, with new_instance's values prevailing in case of conflict
-            session.merge(
-                existing_instance
-            )  # the new details will override the old  ones
-        else:
-            raise ValueError("not predicted state change transition")
+            new_instance.latest_prices_array = (
+                new_instance.latest_prices_array
+                if new_instance.latest_prices_array
+                else existing_instance.latest_prices_array
+            )
+
+        else:  # no change in considered attributes (extra attributes might have changed. we will store new version if key collision.)
+            transition_type = None
+
+        new_instance.extra_attributes = {  # Merge dictionaries, with new_instance's values prevailing in case of conflict
+            **existing_instance.extra_attributes,
+            **new_instance.extra_attributes,
+        }
+        session.merge(new_instance)  # the new details will override the old  ones
     else:  # we do not have any record yet for this day
+        transition_type = "NEW_DATE_RECORDED"
         session.add(new_instance)
+    if transition_type:
+        calendar_day_transition = AirBnbRoomCalendarDayTransition(
+            room_id=new_instance.room_id,
+            calendar_day=new_instance.calendar_day,
+            transition_type=transition_type,
+            state=new_instance.state,
+            price=new_instance.price,
+            latest_prices_array=new_instance.latest_prices_array,
+            minimum_stay_nights=new_instance.minimum_stay_nights,
+            cleaning_fee=new_instance.cleaning_fee,
+            currency=new_instance.currency,
+            extra_attributes=new_instance.extra_attributes,
+        )
+        session.add(calendar_day_transition)
